f89bb3707aba497e70a81a092b378ca20ca2536d
Horea Oros <horea.oros@gmail.com>
2019-10-08 11:52:23 +0300

    add read number for console

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -11,7 +11,29 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World of C#!");
-            Console.Beep(1000, 2000);
+            Console.Beep(1000, 1000);
+
+            int n; // [-2^31 -- > 2^31 - 1]
+
+            string line;
+
+
+            Console.WriteLine("Introduceti un numar: ");
+            line = Console.ReadLine();
+
+            try
+            {
+                n = int.Parse(line);
+                Console.WriteLine("Ati introdus: {0}", n);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Nu ati introdus un numar");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Numarul introdus nu este in limitele lui int");
+            }
         }
     }
 }
