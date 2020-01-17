11ab5fda5c91a3e883903b74fd64e54bcfa7e1f4
Horea Oros <horea.oros@gmail.com>
2020-01-17 12:12:10 +0200

    add 1401

---
+++ 1401/DataCalendaristica.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _1401
+{
+    class DataCalendaristica
+    {
+        private int zi;
+        private int luna;
+        private int an;
+
+        // constructor
+        public DataCalendaristica(int zi, int luna, int an)
+        {
+            this.zi = zi;
+            this.luna = luna;
+            this.an = an;
+        }
+
+        internal int Zile(DataCalendaristica d)
+        {
+            int count = 0;
+
+            while (!this.EsteEgalCu(d))
+            {
+                d.ScadOZi();
+                count++;
+            }
+
+            return count;
+        }
+
+        private void ScadOZi()
+        {
+            if (this.zi > 1)
+            {
+                this.zi--;
+            }
+            else
+                switch(this.luna)
+                {
+                    case 1:
+                        this.zi = 31;
+                        this.luna = 12;
+                        this.an--;
+                        break;
+                    case 2:
+                    case 4:
+                    case 6:
+                    case 8:
+                    case 9:
+                    case 11:
+                        this.zi = 31;
+                        this.luna--;
+                        break;
+                    case 3:
+                        this.luna--;
+                        if ((an % 4 == 0 && an % 100 != 0) || an % 400 == 0)
+                            this.zi = 29;
+                        else
+                            this.zi = 28;
+                        break;
+                    case 5:
+                    case 7:
+                    case 10:
+                    case 12:
+                        this.luna--;
+                        this.zi = 30;
+                        break;
+                }
+        }
+
+        private bool EsteEgalCu(DataCalendaristica d)
+        {
+            return this.zi == d.zi && this.luna == d.luna && this.an == d.an;
+            //if (this.zi == d.zi && this.luna == d.luna && this.an == d.an)
+            //{
+            //    return true;
+            //}
+            //else
+            //    return false;
+        }
+
+        public override string ToString()
+        {
+            return zi.ToString() + "-" + luna.ToString() + "-" + an.ToString();
+        }
+    }
+}
---
+++ 1401/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataCalendaristica d1 = new DataCalendaristica(14, 1, 2020);
+            DataCalendaristica d2 = new DataCalendaristica(1, 1, 2001);
+
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            // afisam numarul de zile dintre cele doua date calendaristice
+            Console.WriteLine(d2.Zile(d1));
+
+
+            // sortare prin numarare (counting sort)
+            int[] v = new int[10000000];
+            int max = 100;
+            Random rnd = new Random();
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = rnd.Next(max);
+            }
+            //for (int i = 0; i < v.Length; i++)
+            //{
+            //    Console.Write($"{v[i]} ");
+            //}
+
+
+            // vector de frecventa;
+            int[] f = new int[max];
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                f[v[i]]++;
+            }
+            for (int i = 0; i < f.Length; i++)
+            {
+                Console.Write($"{f[i]} ");
+            }
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < f[i]; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
